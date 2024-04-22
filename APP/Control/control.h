2cc6ea33b4fd8733597e1c912a9e66dff9e5fbb1
imurbed <3054882303@qq.com>
2024-04-22 20:34:54 +0800

    Update control.h

--- APP/Control/control.h
+++ APP/Control/control.h
@@ -10,14 +10,10 @@
 
 void Turn_left(void);
 void Turn_right(void);
-<<<<<<< HEAD
 uint16_t PID_Increasement(int8_t Expect_Encode_Value,int8_t num);
 
 
 
-=======
-void Move_forward(u8 level);
->>>>>>> bc80c2b874f3006fa20833abba5bcb1ffd66cce1
 
 #endif
 
